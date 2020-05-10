330db97865c848534f40405400ace5d12db6bf85
anasali2193 <anasali2193@gmail.com>
2020-05-10 06:33:37 +0500

    add checking functionality wtih handleAnswer

--- managing-state/src/App.js
+++ managing-state/src/App.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Game from './Game';
+import Score from './Score';
 import logo from './logo.svg';
 import './App.css';
-import Fetch from './Fetch';
 
-class App extends React.Component{
-  state ={
-    mobile : [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        },
-        {
-          "id": "ghi",
-          "name": "gjgjsksk",
-          },
-    ]
+
+class App extends Component {
+   state = {
+    numAnswer: 0,
+    numQuestions: 0,
+  };
+
+handleAnswer = (answerWasCorrect) =>{
+  if (answerWasCorrect){
+  this.setState((curState)=> ({
+  numAnswer : curState.numAnswer + 1 ,
+  }))
   }
+  else{
+  this.setState((curState) =>({
+  numQuestions : curState.numQuestions + 1,
+  }));
+  }
+
+}
  
 
+  
+  render() {
 
-
-  render (){
-  return (
-    <div className="App">
-   <ul> 
-   {this.state.mobile.map((mob)=>
-   (
-      <li key= {mob.id}>{mob.name}{mob.id}</li>
-    
-    ))}
-
-    </ul>
-  <Fetch mobile = {this.state.mobile}/>
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+    <Game handleAnswer ={this.handleAnswer} />
+    <Score 
+numQuestions ={this.state.numQuestions}
+numAnswer = {this.state.numAnswer}
+/>
+      </div>
+    );
   }
 }
 
